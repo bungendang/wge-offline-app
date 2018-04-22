e5b11282dcdeb750b81e1f0a955a790f6247982a
Endang Kurniawan <bungendang@gmail.com>
2018-04-22 19:25:45 +0800

    add v4

--- main.js
+++ main.js
@@ -1,60 +1,90 @@
 // Inital app
-const { app, BrowserWindow, dialog } = require('electron')
+const { app, BrowserWindow, dialog , Menu, protocol, ipcMain} = require('electron')
 const updater = require("electron-updater");
 const autoUpdater = updater.autoUpdater;
 
 let path = require('path')
 
+let template = []
+if (process.platform === 'darwin') {
+  // OS X
+  const name = app.getName();
+  template.unshift({
+    label: name,
+    submenu: [
+      {
+        label: 'About ' + name,
+        role: 'about'
+      },
+      {
+        label: 'Quit',
+        accelerator: 'Command+Q',
+        click() { app.quit(); }
+      },
+    ]
+  })
+}
+
 let win;
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToWindow('Checking for update...');
+})
+autoUpdater.on('update-available', (info) => {
+  sendStatusToWindow('Update available.');
+})
+autoUpdater.on('update-not-available', (info) => {
+  sendStatusToWindow('Update not available.');
+})
+autoUpdater.on('error', (err) => {
+  sendStatusToWindow('Error in auto-updater. ' + err);
+})
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  sendStatusToWindow(log_message);
+})
+autoUpdater.on('update-downloaded', (info) => {
+  sendStatusToWindow('Update downloaded');
+});
+app.on('ready', function() {
+  // Create the Menu
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
 
-///////////////////
-// Auto upadater //
-///////////////////
-autoUpdater.requestHeaders = { "PRIVATE-TOKEN": "Personal access Token" };
-autoUpdater.autoDownload = true;
-
-autoUpdater.setFeedURL({
-    provider: "generic",
-    url: "https://gitlab.com/_example_repo_/-/jobs/artifacts/master/raw/dist?job=build"
+  createDefaultWindow();
+});
+app.on('window-all-closed', () => {
+  app.quit();
 });
 
-autoUpdater.on('checking-for-update', function () {
-    sendStatusToWindow('Checking for update...');
-});
+//
+// CHOOSE one of the following options for Auto updates
+//
 
-autoUpdater.on('update-available', function (info) {
-    sendStatusToWindow('Update available.');
+//-------------------------------------------------------------------
+// Auto updates - Option 1 - Simplest version
+//
+// This will immediately download an update, then install when the
+// app quits.
+//-------------------------------------------------------------------
+app.on('ready', function()  {
+  autoUpdater.checkForUpdatesAndNotify();
 });
-
-autoUpdater.on('update-not-available', function (info) {
-    sendStatusToWindow('Update not available.');
-});
-
-autoUpdater.on('error', function (err) {
-    sendStatusToWindow('Error in auto-updater.');
-});
-
-autoUpdater.on('download-progress', function (progressObj) {
-    let log_message = "Download speed: " + progressObj.bytesPerSecond;
-    log_message = log_message + ' - Downloaded ' + parseInt(progressObj.percent) + '%';
-    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
-    sendStatusToWindow(log_message);
-});
-
-autoUpdater.on('update-downloaded', function (info) {
-    sendStatusToWindow('Update downloaded; will install in 1 seconds');
-});
-
-autoUpdater.on('update-downloaded', function (info) {
-    setTimeout(function () {
-        autoUpdater.quitAndInstall();
-    }, 1000);
-});
-
-autoUpdater.checkForUpdates();
 
 function sendStatusToWindow(message) {
     console.log(message);
+}
+
+
+function createDefaultWindow() {
+  win = new BrowserWindow();
+  win.webContents.openDevTools();
+  win.on('closed', () => {
+    win = null;
+  });
+  win.loadURL(`file://${__dirname}/app/index.html#v${app.getVersion()}`);
+  return win;
 }
 
 
@@ -64,12 +94,13 @@ function createWindow () {
     width: 1280, 
     height: 768,
     backgroundColor: '#ffffff',
-    icon: path.join(__dirname,'src/favicon.ico'),
+    icon: path.join(__dirname,'app/favicon.ico'),
     webPreferences: {
       plugins: true,
       webSecurity:false
     }
     // icon: path.join(__dirname,'dist/assets/icons/logo-app-1024.icns') //only for mac
+    // icon: path.join(__dirname,'app/wg-logo-app.png') //for linux
   })
 
   // win.openDevTools();
@@ -93,22 +124,22 @@ function printPdf(){
 
 
 // Create window on electron intialization
-app.on('ready', createWindow)
+// app.on('ready', createWindow)
 
 
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
+// // Quit when all windows are closed.
+// app.on('window-all-closed', function () {
 
-  // On macOS specific close process
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+//   // On macOS specific close process
+//   if (process.platform !== 'darwin') {
+//     app.quit()
+//   }
+// })
 
-app.on('activate', function () {
-  // macOS specific close process
-  if (win === null) {
-    createWindow()
-  }
-})
+// app.on('activate', function () {
+//   // macOS specific close process
+//   if (win === null) {
+//     createWindow()
+//   }
+// })
